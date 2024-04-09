c634bac1f9016989ee429a4dc0806dee978262c6
Papp Adam <adam.papp123@gmail.com>
2024-04-09 15:20:12 +0200

    Finished for now

--- main.py
+++ main.py
@@ -1,37 +1,66 @@
 import os
 import concurrent.futures as f
 import re
+import csv
 
 def list_txt_files():
     return [f for f in os.listdir() if f.endswith('.txt')]
 
 def process_file(filename):
     print(f"Processing {filename}\n")
-    try:
-        with open(filename, 'r') as file:
-            data = file.read()
-            i = 0
-            for line in data.split('\n'):
-                i += 1
-                match = re.search(r'(rb\-.*\#)|(sb\-.*\#)')
-                if "sh version" in line and "sh version | i Processor" not in line:
-                    try:
-                        for j in range(i, len(data.split('\n'))-1):
-                            if "System image file is" in data.split('\n')[j]:
-                                print(data.split('\n')[j])
-                            if "Device#" in data.split('\n')[j]:
-                                print("PID: " + data.split('\n')[j+2].split()[1])
-                                print("VID: " + data.split('\n')[j+2].split()[2])
-                            if data.split('\n')[j].match(match):
-                                print(data.split('\n')[j])
-                                i += j
+    output_filename = filename.split(".")[0] + "_out.csv"
+    with open(output_filename, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Hostname", "Serial Number", "Version", "Model", "Image"])
+    
+        try:
+            with open(filename, 'r') as file:
+                data = file.read().splitlines()
+                for i in range(len(data)):
+                    if data[i].startswith('Cisco IOS Software') and data[i-1].endswith('sh version'):
+                        j = i + 1
+                        
+                        while j < len(data):
+                            general_pattern = re.compile(r".*b-.+#$")
+                            if general_pattern.match(data[j]):
                                 break
-                    except IndexError:
-                        print("This is the last line of the file.\n")
-                
-        pass
-    except Exception as e:
-        print(f"An error occurred while processing {filename}: {e} \n")
+
+                            j += 1
+                    
+                        
+                        switch_pattern_1 = re.compile(r"sb-.+#$")
+                        router_pattern_1 = re.compile(r"rb-.+#$")
+                        hostname = data[j].strip("#")
+                        if switch_pattern_1.match(data[j]) or router_pattern_1.match(data[j]):
+                            version = ""
+                            serial = ""
+                            model = ""
+                            image = ""
+                            for k in range(i, j):
+                                if "Cisco IOS Software" in data[k]:
+                                    version = data[k].split(",")[2].strip("Version ")
+                                    print(f"Version: {version}")
+                                if "Motherboard serial number" in data[k] and not router_pattern_1.match(data[j]):
+                                    serial = {data[k].split(":")[-1].strip(" ").strip("{").strip("}").strip("'")}
+                                    print(f"Serial: {serial}")
+                                elif "License UDI:" in data[k] and router_pattern_1.match(data[j]):
+                                    serial = data[k + 5][29:].strip(" ")
+                                    print(f"Serial: {serial}")
+                                if "K bytes of memory" in data[k]:
+                                    print(i+k)
+                                    model = data[k].split(" ")[1]
+                                    print(f"Model: {model}")
+                                if "System image file is" in data[k]:
+                                    image = data[k].split("is ")[-1].strip('"')
+                                    print(f"Image: {image}")
+                            
+                            writer.writerow([hostname, serial, version, model, image])
+                                
+                        
+                        
+            pass
+        except Exception as e:
+            print(f"An error occurred while processing {filename}: {e} \n")
 
 def process_txt_files_in_parallel():
     txt_files = list_txt_files()
